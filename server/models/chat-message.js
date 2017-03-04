05f9fa2353631a5164b46ab3abaee12b6af417e0
iamvanja <vanja@gavric.org>
2017-03-04 04:33:15 -0700

    Added basic socket.io implementation (emit message after it is persisted and append client object to it)

--- server/models/chat-message.js
+++ server/models/chat-message.js
@@ -2,4 +2,30 @@
 
 module.exports = function(Chatmessage) {
 
+  Chatmessage.observe('after save', function(ctx, next) {
+    // saved
+    if (ctx.instance) {
+      // console.log('Saved %s#%s', ctx.Model.modelName, ctx.instance.id);
+      // find client and include (owner of this message)
+      Chatmessage.findById(
+        ctx.instance.id, {
+          include: { relation: 'client' }
+        },
+        function(err, message) {
+          if (err) {
+            next(err);
+          }
+          // emit message to the client side
+          Chatmessage.app.io.emit('message:new', message);
+        });
+    }
+    // updated
+    else {
+      console.log('Updated %s matching %j',
+        ctx.Model.pluralModelName,
+        ctx.where);
+    }
+    next();
+  });
+
 };
--- server/server.js
+++ server/server.js
@@ -24,6 +24,11 @@ boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) {
+    // app.start();
+    app.io = require('socket.io')(app.start());
+    app.io.on('connection', function(socket){
+      console.log('a user connected');
+    });
+  }
 });
